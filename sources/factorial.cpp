ff9e57f3d26e056ae5601395e66ba3ef260bc4b2
Evan LOISEAU <eloiseau@sairem.com>
2022-06-29 09:13:24 +0200

    clang-14 + -b branch

--- sources/factorial.cpp
+++ sources/factorial.cpp
@@ -7,6 +7,9 @@
     const ::std::uint8_t n
 ) -> ::std::uint64_t
 {
+    if (n == 100) {
+        return 0;
+    }
     return n == 0 || n == 1 ? 1 : static_cast<::std::uint64_t>(n) * factorial(n - 1);
 }
 
