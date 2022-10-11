836dd585dacf2e2695668bc0d30a4fd316c6be51
DiantArts <DiantArts@gmail.com>
2022-10-11 09:39:29 -0700

    one by one

--- sources/Index.cmake
+++ sources/Index.cmake
@@ -5,7 +5,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/Sanitizers.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/CompilerWarnings.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/CompilerFlags.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/StaticAnalyzers.cmake) # possible to add more static analyzers in the file
-# include(${CMAKE_CURRENT_LIST_DIR}/Shaders.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/Shaders.cmake)
 # include(${CMAKE_CURRENT_LIST_DIR}/Dependencies.cmake)
 # include(${CMAKE_CURRENT_LIST_DIR}/Cache.cmake)
 # include(${CMAKE_CURRENT_LIST_DIR}/Documentation.cmake) # documentation configuration in the file
