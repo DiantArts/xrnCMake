12a5a92eaf592cb5eecf6ba8aad8e0dc476f6956
DiantArts <DiantArts@gmail.com>
2022-10-11 09:37:58 -0700

    one by one

--- sources/Index.cmake
+++ sources/Index.cmake
@@ -1,6 +1,6 @@
 include_guard()
 
-# include(${CMAKE_CURRENT_LIST_DIR}/StandardProjectSettings.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/StandardProjectSettings.cmake)
 # include(${CMAKE_CURRENT_LIST_DIR}/Sanitizers.cmake)
 # include(${CMAKE_CURRENT_LIST_DIR}/CompilerWarnings.cmake)
 # include(${CMAKE_CURRENT_LIST_DIR}/CompilerFlags.cmake)
